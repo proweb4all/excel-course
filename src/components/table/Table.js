2d642300d3edd9354cd2d511bbe742d4ae5ea453
proweb4all <proweb4all@narod.ru>
2021-01-22 15:29:36 +0300

    start of development of resize functionality

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -3,7 +3,24 @@ import {createTable} from '@/components/table/table.template';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
+  constructor($root) {
+    super($root, {
+      listeners: ['click', 'mousedown', 'mousemove', 'mouseup']
+    })
+  }
   toHTML() {
     return createTable(16)
+  }
+  onClick() {
+    console.log('click')
+  }
+  onMousedown() {
+    console.log('mousedown')
+  }
+  onMousemove() {
+    console.log('mousemove')
+  }
+  onMouseup() {
+    console.log('mouseup')
   }
 }
